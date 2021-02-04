4c58c9ada197583d778f97ffc31d860b58e8ad81
EricFormar <66625345+EricFormar@users.noreply.github.com>
2021-02-04 08:40:00 -0300

    reordena estilos

--- RHCGapp/controllers/indexController.js
+++ RHCGapp/controllers/indexController.js
@@ -2,8 +2,7 @@ module.exports = {
     index: (req,res) =>{
         res.render("index", {
             title: "home",
-            css: "mobile.css",
-            body: "style.css"
+            css: "index.css",
         });
     }
 }
